17444ca3f7818c4df3c5d52527118666a5547b38
Kaj Ström <kaj.strom@kapsi.fi>
2018-06-04 07:58:25 +0300

    Fix

--- index.js
+++ index.js
@@ -98,6 +98,6 @@ app.get("/info", (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(3001, () => {
-    console.log("Server running on port 3001")
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT)
 })
